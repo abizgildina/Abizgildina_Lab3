a99dbb20bc00df7bc4e086493f47085f7a10a091
abizgildina <39943314+abizgildina@users.noreply.github.com>
2021-12-24 01:45:23 +0500

    обнова индекса

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 </head>
 
 <body>
+    <h1>ЛР3</h1>
     <a href="f1.html">Упражнение 3-1</a> <br>
     <a href="f2.html">Упражнение 3-2</a> <br>
     <a href="f3.html">Упражнение 3-3</a> <br>
@@ -26,6 +27,9 @@
     <a href="s3-4.php">Задача 3.4</a> <br>
     <a href="s3-5.php">Задача 3.5</a> <br>
     <a href="s3-6.html">Задача 3.6</a> <br>
+    <br><br>
+    <a href="bd_user/indexLR4.php"><h1> Переход к ЛР4</h1></a> <br>
+    
 </body>
 
 </html>
